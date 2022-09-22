477184c06b0a748a7a728b592f2e65c218b9fb17
Masira97 <naashmasira@gmail.com>
2022-09-22 03:32:36 -0700

    0-

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,10 +3,11 @@
 #include <stdio.h>
 
 /**
- *void prototypes()
- *int prototypes()
- *char prototypes()
+ * void prototypes()
+ * int prototypes()
+ * char prototypes()
  */
+
 void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
