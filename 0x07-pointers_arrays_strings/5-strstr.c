cf409bc08167092a6847fdb21454ab33192c9274
Masira97 <naashmasira@gmail.com>
2022-09-27 05:29:34 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,12 +2,12 @@
 
 /**
  * _strstr - Locates a substring.
- * @haystack: The string to be searched.
+ * @haystack: The string to be searched
  * @needle: The substring to be located.
  *
  * Return: If the substring is located - a pointer to the beginning
- * of the located substring.
- * If the substring is not located - NULL.
+ *	of the located substring.
+ *	 If the substring is not located - NULL.
  */
 
 char *_strstr(char *haystack, char *needle)
@@ -19,17 +19,16 @@ char *_strstr(char *haystack, char *needle)
 
 	while (*haystack)
 	{
-		index = 0
+		index = 0;
 
 		if (haystack[index] == needle[index])
 		{
 			do {
 				if (needle[index + 1] == '\0')
 					return (haystack);
-
 				index++;
 
-			} while (haystack[index] == needle[index]
+			} while (haystack[index] == needle[index]);
 		}
 
 		haystack++;
